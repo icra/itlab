618fdb8af262ff66ae5f41c53adaa153ee71a45d
Lluís Bosch <holalluis@gmail.com>
2023-03-06 15:52:06 +0100

    first

---
+++ curl_icra_public_repos.sh
@@ -0,0 +1,7 @@
+#amb 55 repositoris, hi ha 3 pàgines
+
+curl \
+  -H "Accept: application/vnd.github+json" \
+  -H "Authorization: Bearer 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"\
+  -H "X-GitHub-Api-Version: 2022-11-28" \
+  "https://api.github.com/orgs/icra/repos?page=1"
