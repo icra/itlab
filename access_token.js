806a99e3525d32e92229c4aa614399dceafbf3b7
Lluís Bosch <holalluis@gmail.com>
2023-03-07 13:53:31 +0100

    update

---
+++ access_token.js
@@ -0,0 +1,7 @@
+//headers per fetch calls a api github
+//la clau no té permisos
+//només pot llegir repositoris públics
+let headers={
+  "Accept":"application/vnd.github+json",
+  "Authorization":"Bearer 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",
+};
---
+++ draft/curl_repos.sh
@@ -0,0 +1,5 @@
+curl \
+  -H "Accept: application/vnd.github+json" \
+  -H "Authorization: Bearer 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"\
+  -H "X-GitHub-Api-Version: 2022-11-28" \
+  "https://api.github.com/orgs/icra/repos?page=1"
