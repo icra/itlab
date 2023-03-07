9c354d27bb3b2afbdf14adc5acf9f2478982a3b7
Lluís Bosch <holalluis@gmail.com>
2023-03-07 14:05:19 +0100

    update

--- access_token.js
+++ access_token.js
@@ -3,5 +3,5 @@
 //només pot llegir repositoris públics
 let headers={
   "Accept":"application/vnd.github+json",
-  "Authorization":"Bearer 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",
+  "Authorization":"Bearer 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",
 };
--- deploy.sh
+++ deploy.sh
@@ -6,4 +6,13 @@ git commit -am 'update'
 git push
 
 #desplega al servidor
-ssh root@icra.loading.net "cd /var/www/vhosts/icra.cat/itlab.icra.cat/itlab; git pull"
+#ssh root@icra.loading.net "cd /var/www/vhosts/icra.cat/itlab.icra.cat/itlab; git pull"
+
+#servidor
+servidor='icra.loading.net' #loading
+#ruta carpeta servidor
+carpeta="/var/www/vhosts/icra.cat/itlab.icra.cat"
+
+#exclou base de dades
+#exclou ruta de la base de dades
+rsync -hvr . root@$servidor:$carpeta
