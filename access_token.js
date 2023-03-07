cb51228b4a0d5312ea6f14039d9b25a3487c998d
Lluís Bosch <holalluis@gmail.com>
2023-03-07 14:09:31 +0100

    update

--- access_token.js
+++
@@ -1,7 +0,0 @@
-//headers per fetch calls a api github
-//la clau no té permisos
-//només pot llegir repositoris públics
-let headers={
-  "Accept":"application/vnd.github+json",
-  "Authorization":"Bearer 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",
-};
